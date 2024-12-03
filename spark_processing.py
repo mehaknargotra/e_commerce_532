cf8a71302b8d3c9337497d7df827769dcf0c2e96
dhruvin5 <dhruvingandhi5@gmail.com>
2024-12-03 18:13:08 -0500

    elasticsearch added

--- spark_processing.py
+++ spark_processing.py
@@ -16,10 +16,12 @@ logger = logging.getLogger(__name__)
 
 # Initialize Spark Session with appropriate configurations for Ecommerce Data Analysis
 spark = SparkSession.builder \
-    .appName("Ecommerce Data Analysis") \
+    .appName("Ecommerce Data Pipeline") \
+    .config("spark.es.nodes", "localhost") \
     .config("spark.es.port", "9200") \
     .config("spark.es.nodes.wan.only", "true") \
     .getOrCreate()
+
 spark.sparkContext.setLogLevel("ERROR")
             
 kafka_bootstrap_servers = os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'localhost:9092')
@@ -46,11 +48,21 @@ customerDF = (spark.readStream
               .select("data.*")
               .withWatermark("last_login", "2 hours") 
              )
-
+customerDF = customerDF.withColumn("@timestamp", col("last_login"))
 query = customerDF.writeStream \
     .outputMode("append") \
     .format("console") \
     .start()
+
+customerDF.writeStream \
+    .outputMode("append") \
+    .format("org.elasticsearch.spark.sql") \
+    .option("checkpointLocation", "/tmp/spark/checkpoints/customer_analysis") \
+    .option("es.nodes", "localhost") \
+    .option("es.port", "9200") \
+    .option("es.resource", "customer_index") \
+    .start() \
+    .awaitTermination()
 
 # Wait for any of the streams to finish
 spark.streams.awaitAnyTermination()
